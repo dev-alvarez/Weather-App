aeaddebfb6b139edf243e09a83b4738ba35da98b
JoseAlvarez <144159071+JOSEALVAREZ777@users.noreply.github.com>
2024-06-24 14:51:08 -0600

    general

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
+require('dotenv').config();
+
 document.getElementById('searchButton').addEventListener('click', function() {
     const city = document.getElementById('city').value;
   if(city) {
     fetchWeather(city);
   }
 });
+
+function fetchWeather(city) {
+  const apiKey = process.env.API_KEY;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+}
