3ff1480eb89136e679eb7451cc72b77a2e0ec205
JoseAlvarez <144159071+JOSEALVAREZ777@users.noreply.github.com>
2024-06-24 17:29:33 -0600

    a

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+
 document.getElementById('searchButton').addEventListener('click', function() {
     const city = document.getElementById('city').value;
   if(city) {
@@ -11,4 +12,29 @@ function fetchWeather(city) {
   const apiKey = process.env.API_KEY;
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
+  fetch(apiUrl)
+    .then(response => response.json())
+  .then(data => {
+      displayWeather(data);
+    })
+  .cath(error => {
+      console.error('God Knows What Happend, I could not get the data I am so sorry:', error);
+      alert('Not Working, Try Again!');
+    })
+}
+
+function displayWeather(data) {
+  if(data.error) {
+    alert('YOUR CITY IS NOT REAL');
+    return;
+  }
+
+  const weatherDisplay = document.getElementById('weatherDisplay');
+  weatherDisplay.innerHTML =`
+    <h2>${data.name}, ${data.sys.country}</h2>
+    <p><strong>Temperature:</strong> ${data.main.temp}°C</p>
+    <p><strong>Weather:</strong> ${data.weather[0].description}</p>
+    <p><strong>Humidity:</strong> ${data.main.humidity}%</p>
+    <p><strong>Wind Speed:</strong> ${data.wind.speed} m/s</p>
+    `;
 }
